410096ebd86cae37df63099e058128ba7e96d68f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-29 22:15:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,20 @@
 def second_challenge
   epic_tragedy => {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero  => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero  => {},
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :heroine_friends => []
       }
     
   }
-  }
+  
 
   
 end
